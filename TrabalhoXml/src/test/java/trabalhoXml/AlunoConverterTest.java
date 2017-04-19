9aa08270eec089bf65c894410bf12cc3f80b4980
lucas <lucas@samsung>
2017-04-19 09:31:30 -0300

    Atualização

--- TrabalhoXml/src/test/java/trabalhoXml/AlunoConverterTest.java
+++ TrabalhoXml/src/test/java/trabalhoXml/AlunoConverterTest.java
@@ -1,47 +1,25 @@
 package trabalhoXml;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 
-import converter.AlunoXmlConverter;
-import converter.PessoaXmlConverter;
 import converter.TurmaXmlConverter;
 import model.Aluno;
-import model.Disciplina;
-import model.Nota;
-import model.Pessoa;
 import model.Turma;
 
 public class AlunoConverterTest {
 
 	List<Aluno> alunos;
-	
-	
+
 	@Test
-	public void deveConverterParaFileXML() throws URISyntaxException {
+	public void deveConverterParaFileXML() {
 		TurmaXmlConverter converter = new TurmaXmlConverter();
-		Turma turma = new Turma();
-		String xml = converter.toXML(turma);
-		File file = new File("novaAluno.xml");
-		converter.toXML(turma, file);
-		assertTrue(Files.exists(Paths.get("novoAluno.xml")));
-		System.out.println(xml);
-		
-		
-		PessoaXmlConverter converter = new PessoaXmlConverter();
-		Pessoa pessoa = new Pessoa("Lucas", "123546");
-		String xml = converter.toXML(pessoa);
-		File file = new File("novaPessoa.xml");
-		converter.toXML(pessoa, file);
-		assertTrue(Files.exists(Paths.get("novaPessoa.xml")));
-		System.out.println(xml);
+		Turma turma = converter.toObject(new File("/home/lucas/Downloads/boletimTurma.xml"));
+		assertNotNull(turma);
+		System.out.println("boletimTurma.xml");
 	}
 }
