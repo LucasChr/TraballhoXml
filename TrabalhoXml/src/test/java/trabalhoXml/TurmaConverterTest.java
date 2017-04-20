7fca7464aff0634636415186b112330a8bdd3085
lucas <lucas@samsung>
2017-04-19 21:57:23 -0300

    Atualização

--- TrabalhoXml/src/test/java/trabalhoXml/TurmaConverterTest.java
+++ TrabalhoXml/src/test/java/trabalhoXml/TurmaConverterTest.java
@@ -1,14 +1,7 @@
 package trabalhoXml;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 import org.junit.Test;
@@ -23,65 +16,43 @@ public class TurmaConverterTest {
 
 	File file = new File("/opt/boletimTurma.xml");
 
-	/*@Test
-	public void deveConverterParaTurma() {
-		TurmaXmlConverter converter = new TurmaXmlConverter();
-		Turma turma = converter.toObject(file);
-		assertNotNull(turma);
-		System.out.println(turma.getAlunos());
-	}*/
-
 	@Test
 	public void deveConverterParaXML() throws URISyntaxException {
 		TurmaXmlConverter converter = new TurmaXmlConverter();
 		Turma turma = converter.toObject(file);
-		for(int i=0;i<=turma.getAlunos().size();i++){
-			Aluno a = turma.getAlunos().get(i);
-			String nome = a.getNome();
-			
-			for(int d=0;d<=a.getDisciplinas().size();d++){
-				Disciplina disc = a.getDisciplinas().get(d);
-				String materia = disc.getNome();
-				Double soma = null;
-				Integer numNotas = disc.getNotas().size();
-				for(int n=0;n<=disc.getNotas().size();n++){
-					Nota not = disc.getNotas().get(n);
-					Double nota = not.getValor();
-					soma += nota;
+		List<Aluno> t = turma.getAlunos();
+		// System.out.println(t.size());
+		String nome = null;
+		String materia = null;
+		Double soma = 0.0;
+		String auxMateria = null;
+		Double media = 0.0;
+		Double mediaGeral = 0.0;
+		Double auxMedia = 0.0;
+		int num2 = t.size();
+		for (int i = 0; i <= num2; i++) {
+			nome = t.get(i).getNome();
+			List<Disciplina> d = t.get(i).getDisciplinas();
+			Integer num1 = d.size();
+			for (int m = 0; m < num1; m++) {
+				materia = d.get(m).getNome();
+				List<Nota> n = d.get(m).getNotas();
+				Integer num = n.size();
+				for (int a = 0; a < num; a++) {
+					Double aux = n.get(a).getValor();
+					soma = soma + aux;
 				}
-				
+				if (m <= 0) {
+					auxMateria = materia;
+					auxMedia = soma / num1;
+				}
+				media = soma / num1;
+				soma = 0.0;
 			}
+			mediaGeral = (auxMedia + media) / num2;
+			System.out.println(nome + " - " + auxMateria + " - " + auxMedia + " - " + materia + " - " + media
+					+ " - Média geral: " + mediaGeral);
 		}
-		String xml = converter.toXML(turma);
-		assertFalse(xml.isEmpty());
-		System.out.println(xml);
-	}
 
-	/*
-	 * @Test public void deveConverterParaFileXML() throws URISyntaxException {
-	 * TurmaXmlConverter converter = new TurmaXmlConverter(); Turma turma =
-	 * converter.toObject(new File("/home/lucas/Downloads/boletimTurma.xml"));
-	 * String xml = converter.toXML(turma); File file = new
-	 * File("novaTurma.xml"); converter.toXML(turma, file);
-	 * assertTrue(Files.exists(Paths.get("novaTurma.xml")));
-	 * System.out.println(xml); }
-	 * 
-	 */
-	/*
-	 * @Test public void deveConverterAlunosParaXML() throws URISyntaxException
-	 * { AlunoXmlConverter converter = new AlunoXmlConverter(); Aluno aluno =
-	 * converter.toObject(new File("/home/lucas/Downloads/boletimTurma.xml"));
-	 * Alunos alunos = new Alunos(); alunos.addAluno(aluno); String xml =
-	 * converter.toXML(alunos); assertFalse(xml.isEmpty());
-	 * System.out.println(xml); }
-	 */
-/*
-	@Test
-	public void deveConverter() {
-		TurmaXmlConverter converter = new TurmaXmlConverter();
-		Turma turma = converter.toObject(file);
-		assertNotNull(turma);
-		System.out.println(turma);
 	}
-*/
 }
